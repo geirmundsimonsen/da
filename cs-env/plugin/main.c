43a75378d202316e853fae0e332c84a7d46f920c
Geirmund Simonsen <geirmund.simonsen@gmail.com>
2023-12-31 02:34:40 +0100

    refactor env and add multi-stage envelope variants

--- cs-env/plugin/main.c
+++ cs-env/plugin/main.c
@@ -1,4 +1,5 @@
 #include "csound/csdl.h"
+#include <stdbool.h>
 
 double cubic_function(double x) {
     double x2 = x * x;
@@ -7,57 +8,143 @@ double cubic_function(double x) {
 }
 
 typedef struct {
+  double* v_start;
+  bool v_start_is_audio;
+  double* str;
+  bool str_is_audio;
+  double* cf;
+  bool cf_is_audio;
+  double* v_end;
+  bool v_end_is_audio;
+} env_core_args;
+
+typedef struct {
   OPDS h;
   double *out;
-  double *v1, *str1, *cf1, *v2;
+  double *v1, *str1, *cf1, *v2, *str2, *cf2, *v3, *str3, *cf3, *v4, *str4, *cf4, *v5;
   double env_v;
   double sr_inv;
-} env1;
+  int stage;
+  env_core_args core_args;
+} env;
 
-int env1_init(CSOUND *csound, env1 *p) {
+void env_update_core_args(CSOUND* csound, env* p, double* v_start, double* str, double* cf, double* v_end) {
+  p->core_args.v_start = v_start;
+  p->core_args.v_start_is_audio = csound->GetTypeForArg(v_start)->varTypeName == "a";
+  p->core_args.str = str;
+  p->core_args.str_is_audio = csound->GetTypeForArg(str)->varTypeName == "a";
+  p->core_args.cf = cf;
+  p->core_args.cf_is_audio = csound->GetTypeForArg(cf)->varTypeName == "a";
+  p->core_args.v_end = v_end;
+  p->core_args.v_end_is_audio = csound->GetTypeForArg(v_end)->varTypeName == "a";
+}
+
+double env_process_core(env* p, int idx) {
+  double v_start_v = p->core_args.v_start_is_audio ? p->core_args.v_start[idx] : *p->core_args.v_start;
+  double v_end_v = p->core_args.v_end_is_audio ? p->core_args.v_end[idx] : *p->core_args.v_end;
+  double str_v = p->core_args.str_is_audio ? p->core_args.str[idx] * p->sr_inv : *p->core_args.str * p->sr_inv;
+  double cf_v = p->core_args.cf_is_audio ? p->core_args.cf[idx] : *p->core_args.cf;
+
+  if (str_v < 0.0) {
+    str_v = 0.0;
+  }
+
+  if (cf_v < -4) {
+    cf_v = -4;
+  } else if (cf_v > 4) {
+    cf_v = 4;
+  }
+  
+  if (cf_v >= 0.0) {
+    p->env_v += str_v * (p->env_v * cf_v + cubic_function(cf_v));
+  } else {
+    p->env_v += str_v * ((1.0 - p->env_v) * -cf_v + cubic_function(-cf_v));
+  }
+  
+  return p->env_v * (v_end_v - v_start_v) + v_start_v;
+}
+
+int env_init(CSOUND* csound, env* p) {
   p->sr_inv = 1.0 / csound->GetSr(csound);
+  p->stage = 1;
+  env_update_core_args(csound, p, p->v1, p->str1, p->cf1, p->v2);
 
   return OK;
 }
 
-int env1_process_audio(CSOUND *csound, env1 *p) {
-  double* str_p = p->str1;
-  double* cf_p = p->cf1;
-  double* v_start_p = p->v1;
-  double* v_end_p = p->v2;
-
-  double asig_str = csound->GetTypeForArg(p->str1)->varTypeName == "a";
-  double asig_cf = csound->GetTypeForArg(p->cf1)->varTypeName == "a";
-  double asig_v_start = csound->GetTypeForArg(p->v1)->varTypeName == "a";
-  double asig_v_end = csound->GetTypeForArg(p->v2)->varTypeName == "a";
-
+int env1_process(CSOUND* csound, env* p) {
   for (int i = 0; i < CS_KSMPS; i++) {
-    double v_start_v = asig_v_start ? v_start_p[i] : v_start_p[0];
-    double v_end_v = asig_v_end ? v_end_p[i] : v_end_p[0];
-
     if (p->env_v >= 1.0) {
-      p->env_v = 1.0;
+      *p->out = *p->v2;
     } else {
-      double str_v = asig_str ? str_p[i] * p->sr_inv : str_p[0] * p->sr_inv;
-      double cf_v = asig_cf ? cf_p[i] : cf_p[0];
-
-      if (str_v < 0.0) {
-        str_v = 0.0;
-      }
-
-      if (cf_v < -4) {
-        cf_v = -4;
-      } else if (cf_v > 4) {
-        cf_v = 4;
-      }
-      
-      if (cf_v >= 0.0) {
-        p->env_v += str_v * (p->env_v * cf_v + cubic_function(cf_v));
-      } else {
-        p->env_v += str_v * ((1.0 - p->env_v) * -cf_v + cubic_function(-cf_v));
-      }
+      p->out[i] = env_process_core(p, i);
     }
-    p->out[i] = p->env_v * (v_end_v - v_start_v) + v_start_v;
+  }
+
+  return OK;
+}
+
+int env2_process(CSOUND* csound, env* p) {
+  for (int i = 0; i < CS_KSMPS; i++) {
+    if (p->stage > 2) {
+      *p->out = *p->v3;
+    } else {
+      p->out[i] = env_process_core(p, i);
+      if (p->env_v >= 1.0) {
+        p->stage++;
+        p->env_v = 0.0;
+        printf("stage: %d\n", p->stage);
+        if (p->stage == 2) {
+          env_update_core_args(csound, p, p->v2, p->str2, p->cf2, p->v3);
+        }
+      } 
+    }
+  }
+
+  return OK;
+}
+
+int env3_process(CSOUND* csound, env* p) {
+  for (int i = 0; i < CS_KSMPS; i++) {
+    if (p->stage > 3) {
+      *p->out = *p->v4;
+    } else {
+      p->out[i] = env_process_core(p, i);
+      if (p->env_v >= 1.0) {
+        p->stage++;
+        p->env_v = 0.0;
+        printf("stage: %d\n", p->stage);
+        if (p->stage == 2) {
+          env_update_core_args(csound, p, p->v2, p->str2, p->cf2, p->v3);
+        } else if (p->stage == 3) {
+          env_update_core_args(csound, p, p->v3, p->str3, p->cf3, p->v4);
+        }
+      } 
+    }
+  }
+
+  return OK;
+}
+
+int env4_process(CSOUND* csound, env* p) {
+  for (int i = 0; i < CS_KSMPS; i++) {
+    if (p->stage > 4) {
+      *p->out = *p->v5;
+    } else {
+      p->out[i] = env_process_core(p, i);
+      if (p->env_v >= 1.0) {
+        p->stage++;
+        p->env_v = 0.0;
+        printf("stage: %d\n", p->stage);
+        if (p->stage == 2) {
+          env_update_core_args(csound, p, p->v2, p->str2, p->cf2, p->v3);
+        } else if (p->stage == 3) {
+          env_update_core_args(csound, p, p->v3, p->str3, p->cf3, p->v4);
+        } else if (p->stage == 4) {
+          env_update_core_args(csound, p, p->v4, p->str4, p->cf4, p->v5);
+        }
+      } 
+    }
   }
 
   return OK;
@@ -66,14 +153,44 @@ int env1_process_audio(CSOUND *csound, env1 *p) {
 static OENTRY localops[] = {
   { 
     .opname = "env1",
-    .dsblksiz = sizeof(env1),
+    .dsblksiz = sizeof(env),
     .flags = 0,
     .thread = 3,
     .outypes = "a",
     .intypes = "xxxx",
-    .iopadr = (SUBR) env1_init,
-    .kopadr = (SUBR) env1_process_audio,
-  }
+    .iopadr = (SUBR) env_init,
+    .kopadr = (SUBR) env1_process,
+  },
+  { 
+    .opname = "env2",
+    .dsblksiz = sizeof(env),
+    .flags = 0,
+    .thread = 3,
+    .outypes = "a",
+    .intypes = "xxxxxxx",
+    .iopadr = (SUBR) env_init,
+    .kopadr = (SUBR) env2_process,
+  },
+  { 
+    .opname = "env3",
+    .dsblksiz = sizeof(env),
+    .flags = 0,
+    .thread = 3,
+    .outypes = "a",
+    .intypes = "xxxxxxxxxx",
+    .iopadr = (SUBR) env_init,
+    .kopadr = (SUBR) env3_process,
+  },
+  {
+    .opname = "env4",
+    .dsblksiz = sizeof(env),
+    .flags = 0,
+    .thread = 3,
+    .outypes = "a",
+    .intypes = "xxxxxxxxxxxxx",
+    .iopadr = (SUBR) env_init,
+    .kopadr = (SUBR) env4_process,
+  },
 };
 
 LINKAGE
